23070168b9e87c9e22c776035aabc548bec2a984
automaticdai <automatic.dai@gmail.com>
2021-05-21 03:05:14 +0100

    first version

--- main.py
+++ main.py
@@ -1,22 +1,294 @@
-# type, prio, C(LO), C(MI), C(HI)
+########################################################################################################################
+# Analysis of Mixed-Criticality Approximated Computing
+# Dr. Xiaotian Dai
+# University of York
+########################################################################################################################
+
+import math
+import random
+import pprint
+import logging, sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+#logging.debug()
+#logging.info()
+
+########################################################################################################################
+# Parameters
+########################################################################################################################
+number_of_tasks = 6
+number_of_hi_tasks = math.ceil(number_of_tasks / 2)
+hyperperiod = 10000
+periods = [250, 500, 1000, 2000, 5000, 10000]
+target_util = 0.9
+
+C_low_Mi = 0.8
+C_high_multiplier = 2
+gamma = 0.8
+number_of_trials = 10000
 
 
-# define gamma
+########################################################################################################################
+# Variables
+########################################################################################################################
+# task = type {0:LO, 1:HI}, Ci(LO), Ci(MI), Ci(HI), Ti
+# Di = Ti
+# the taskset is ordered by priority
+taskset = []
+taskidx = []
+taskidx_high = []
+taskidx_low = []
 
 
-# calculate based on gamma
+########################################################################################################################
+# Functions
+########################################################################################################################
+# UUniFast
+def UUniFast(n, U_avg):
+    USet = []
+    sumU = U_avg
+    for i in range(n-1):
+        nextSumU = sumU*math.pow(random.random(), 1/(n-i))
+        USet.append(sumU-nextSumU)
+        sumU = nextSumU
+    USet.append(sumU)
+    return USet
 
+# check low RTA
+def rta_low(i, taskset):
+    Ci = taskset[i]["C_low"]
+    Ti = taskset[i]["T_i"]
+    Di = Ti
+    typei = taskset[i]["Type"]
 
+    Ri = -100000
+    Ri_new = Ci
 
+    while (abs(Ri_new - Ri) > 1):
+        Ri = Ri_new
+        Ij = 0
+        for j in taskidx:
+            typej = taskset[j]["Type"]
+            if j < i:
+                Cj = taskset[j]["C_low"]
+                Tj = taskset[j]["T_i"]
+                Ij += math.ceil(Ri/Tj) * Cj
+        Ri_new = Ci + Ij
 
-# check LO
-def rta_iter():
+        if Ri_new > Di:
+            Ri = Ri_new
+            break
+
+    return Ri
+
+# check low -> mid
+def rta_low_to_mid(i, taskset):
     pass
 
 
-# check MI
+# check mid RTA
+def rta_mid(i, taskset):
+    Ci = taskset[i]["C_low"]
+    Ci_mid = taskset[i]["C_mid"]
+    Ti = taskset[i]["T_i"]
+    Di = Ti
+    typei = taskset[i]["Type"]
+
+    Ri = -100000
+    Ri_new = Ci_mid
+
+    while (abs(Ri_new - Ri) > 1):
+        Ri = Ri_new
+        Ij = 0
+        for j in taskidx:
+            typej = taskset[j]["Type"]
+            if j < i and typej == 0:    # j = hpL(i)
+                Cj_mid = taskset[j]["C_mid"]
+                Tj = taskset[j]["T_i"]
+                Ij += math.ceil(Ri/Tj) * Cj_mid
+            elif j < i and typej == 1:  # j = hpH(i)
+                Cj_mid = taskset[j]["C_mid"]
+                Tj = taskset[j]["T_i"]
+                Ij += math.ceil(Ri/Tj) * Cj_mid
+        Ri_new = Ci_mid + Ij
+
+        if Ri_new > Di:
+            Ri = Ri_new
+            break
+
+    return Ri
 
 
+# check mid -> high
+def rta_mid_to_high(i, taskset):
+    pass
 
 
-# check HI
+# check high RTA
+def rta_high(i, taskset):
+    Ci = taskset[i]["C_low"]
+    Ci_high = taskset[i]["C_high"]
+    Ti = taskset[i]["T_i"]
+    Di = Ti
+    typei = taskset[i]["Type"]
+
+    Ri = -100000
+    Ri_new = Ci_high
+
+    while (abs(Ri_new - Ri) > 1):
+        Ri = Ri_new
+        Ij = 0
+        for j in taskidx:
+            typej = taskset[j]["Type"]
+            if j < i and typej == 1:  # j = hpH(i)
+                Cj = taskset[j]["C_low"]
+                Cj_mid = taskset[j]["C_mid"]
+                Cj_high = taskset[j]["C_high"]
+                Tj = taskset[j]["T_i"]
+                Ij += math.ceil(Ri/Tj) * Cj_high
+        Ri_new = Ci_high + Ij
+
+        if Ri_new > Di:
+            Ri = Ri_new
+            break
+
+    return Ri
+
+
+########################################################################################################################
+# Trial
+########################################################################################################################
+
+def trial():
+    taskset = []
+    taskidx = []
+    taskidx_high = []
+    taskidx_low = []
+
+    # generate the taskset
+    U = UUniFast(number_of_tasks, target_util)
+    #print(U)
+
+    sum_U = 0
+
+    for U_i in U:
+        T_i = random.choice(periods)
+        C_i = math.floor(U_i * T_i)
+        if C_i < 1:
+            C_i = 1
+        sum_U += C_i / T_i
+        task_i = {"Type": 0, "C_low": C_i, "C_mid": 0, "C_high": 0, "T_i": T_i}
+
+        # append through deadline monotonic
+        found = False
+        for j in range(len(taskset)):
+            if T_i <= taskset[j]["T_i"]:
+                taskset.insert(j, task_i)
+                found = True
+                break
+        if not taskset or not found:
+            taskset.append(task_i)
+
+    #print("Total util: ", sum_U)
+
+    # select the HI tasks
+    for i in range(number_of_tasks):
+        taskidx.append(i)
+    idx_high_ = random.sample(taskidx, number_of_hi_tasks)
+
+    for i in range(number_of_tasks):
+        if i in idx_high_:
+            taskset[i]["Type"] = 1
+            taskset[i]["C_high"] = math.ceil(taskset[i]["C_low"] * C_high_multiplier)
+            taskidx_high.append(i)
+        else:
+            taskset[i]["C_mid"] = math.ceil(taskset[i]["C_low"] * C_low_Mi)
+            taskidx_low.append(i)
+
+    # check schedulability of low mode; if not then discard (discard skipped)
+    for i in range(number_of_tasks):
+        Di = taskset[i]["T_i"]
+        Ri = rta_low(i, taskset)
+        if (Ri > Di):
+            #print("Error #1.")
+            pass
+
+
+    # calculate C_i^MD based on gamma
+    for i in taskidx_high:
+        taskset[i]["C_mid"] = taskset[i]["C_low"] + math.ceil((taskset[i]["C_high"] - taskset[i]["C_low"]) * gamma)
+
+    #pprint.pprint(taskset)
+
+
+    # select and overrun one of the HI tasks
+    overrun_task_idx = random.choice(taskidx_high)
+    #print("Overrun idx: ", overrun_task_idx)
+
+
+    # check schedulability of HI or AP
+    overrun_Ci_low = taskset[overrun_task_idx]["C_low"]
+    overrun_Ci_mid = taskset[overrun_task_idx]["C_mid"]
+    overrun_Ci_high = taskset[overrun_task_idx]["C_high"]
+
+    overrun_Ci = int(random.randrange(overrun_Ci_low, overrun_Ci_high))
+    #print("overrun_Ci: ", overrun_Ci)
+
+    # METHOD 1
+    method1_feasible = True
+    # check sched(HI) for low task only
+    for i in taskidx_high:
+        Di = taskset[i]["T_i"]
+        Ri = rta_high(i, taskset)
+        if (Ri > Di):
+            method1_feasible = False
+            #print("Error #2.")
+
+    # METHOD 2
+    method2_feasible = True
+    method2_feasible_high = True
+    low_all = len(taskidx_low)
+    low_count = 0
+    if overrun_Ci > overrun_Ci_mid:
+        # check sched(HI) for high task only
+        for i in taskidx_high:
+            Di = taskset[i]["T_i"]
+            Ri = rta_high(i, taskset)
+            if (Ri > Di):
+                method2_feasible = False
+                #print("Error #3.")
+    else:  # overrun_Ci > taskset[overrun_task_idx]["C_low"]:
+        # check sched(MID) for all tasks
+        for i in taskidx:
+            Di = taskset[i]["T_i"]
+            typei = taskset[i]["Type"]
+            Ri = rta_mid(i, taskset)
+            if (Ri > Di):
+                method2_feasible = False
+                if (typei == 1):
+                    method2_feasible_high = False
+                #print("Error #4.")
+            elif (Ri <= Di and typei == 0):
+                low_count += 1
+    # if high task is not feasible, then the result is invalid
+    if not method2_feasible_high:
+        low_count = 0
+
+    #print(method1_feasible, method2_feasible)
+
+    return (not method1_feasible and method2_feasible, low_count, low_all)
+
+
+########################################################################################################################
+# Main starts here
+########################################################################################################################
+count = 0
+count_low = 0
+count_low_all = 0
+
+for k in range(number_of_trials):
+    ret, low_count, low_all = trial()
+    if ret:
+        count += 1
+    count_low += low_count
+    count_low_all += low_all
+print(count, count_low, count_low_all)
