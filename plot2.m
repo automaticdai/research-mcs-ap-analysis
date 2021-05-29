2366e7f127243cd75f3a60c2e2fe857bdb19ffad
automaticdai <automatic.dai@gmail.com>
2021-05-29 13:43:55 +0100

    update diagram

--- plot2.m
+++ plot2.m
@@ -1,10 +1,22 @@
+close all
+
 trials_num = 10000;
 
 x = data2(:,1);
 y1 = data2(:,2) ./ trials_num;
 y2 = data2(:,3) ./ trials_num;
 
-plot(x, [y1,y2], 'o-')
+
+f = figure();
+
+plot(x, y1, 'o-')
+
+hold on
+
+plot(x, y2, 'x--')
+
+
+
 
 xlabel('Σ Util');
 ylabel('Schedulability');
