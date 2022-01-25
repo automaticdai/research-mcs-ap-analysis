6c44e34f1822b22b6ecdd1a3b33f17cef32e2ce9
automaticdai <automatic.dai@gmail.com>
2022-01-25 01:30:50 +0000

    improvemet and clean

--- main.py
+++ main.py
@@ -13,19 +13,23 @@ import sys
 from util import UUniFast
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)  # logging.debug() / logging.info()
-random.seed(10)  # fix the random seed for reproduciablity
+random.seed(10)  # fix the random seed for reproducibility
 
 ########################################################################################################################
 # Parameters
 ########################################################################################################################
-number_of_tasks = 5     # total number of tasks
-number_of_hi_tasks = 3  # control proportion of hi-tasks to be ~50%
-periods = [250, 500, 1000, 2000, 5000, 10000]  # hyperperiod = 10000
+number_of_tasks = 10     # total number of tasks
+number_of_hi_tasks = 5  # control proportion of hi-tasks to be ~50%
+periods = [100, 200, 250, 500, 1000, 2000, 2500, 5000, 10000]  # hyper-period = 10000
 
 C_low_Mi = 0.8
 C_high_multiplier = 3
 
-number_of_trials = 10000
+number_of_trials = 50
+
+# Mi = 0, 1, 2, 3, 4, 5, ..., 23
+QoC_candidates = [1.0, 0.95, 0.9, 0.85, 0.8]
+C_low_candidates = [1.0, 0.9, 0.8, 0.7, 0.6]
 
 ########################################################################################################################
 # Variables
@@ -250,9 +254,9 @@ def rta_high(i, taskset):
 
 
 ########################################################################################################################
-# Trial
+# Trials
 ########################################################################################################################
-def trial(target_util, gamma):
+def task_gen(target_util):
     taskset = []
     taskidx = []
     taskidx_high = []
@@ -306,172 +310,81 @@ def trial(target_util, gamma):
             # print("Error #1.")
             pass
 
-    # calculate C_i^MD based on gamma
-    for i in taskidx_high:
-        taskset[i]["C_mid"] = taskset[i]["C_low"] + math.ceil((taskset[i]["C_high"] - taskset[i]["C_low"]) * gamma)
-
-    # pprint.pprint(taskset)
-
-    # select and overrun one of the HI tasks
-    overrun_task_idx = random.choice(taskidx_high)
-    # print("Overrun idx: ", overrun_task_idx)
-
-    # check schedulability of HI or AP
-    overrun_Ci_low = taskset[overrun_task_idx]["C_low"]
-    overrun_Ci_mid = taskset[overrun_task_idx]["C_mid"]
-    overrun_Ci_high = taskset[overrun_task_idx]["C_high"]
-
-    overrun_Ci = int(random.randrange(overrun_Ci_low, overrun_Ci_high))
-    # print("overrun_Ci: ", overrun_Ci)
-
-    # METHOD 1
-    method1_feasible = True
-    # check sched(HI) for low-to-high
-    for i in taskidx_high:
-        Di = taskset[i]["T_i"]
-        Ri = rta_low_to_high(i, taskset)
-        if (Ri > Di):
-            method1_feasible = False
-            # print("Error #2.")
-
-    # METHOD 2
-    method2_feasible = True
-    method2_feasible_high = True
-    low_all = len(taskidx_low)
-    low_count = 0
-    if overrun_Ci > overrun_Ci_mid:
-        # check sched(HI) for high task only
-        for i in taskidx_high:
-            Di = taskset[i]["T_i"]
-            Ri = rta_mid_to_high(i, taskset)
-            if (Ri > Di):
-                method2_feasible = False
-                # print("Error #3.")
-    else:  # overrun_Ci > taskset[overrun_task_idx]["C_low"]:
-        # check sched(MID) for all tasks, and sched(LOW-MID) for high
-        for i in taskidx:
-            Di = taskset[i]["T_i"]
-            typei = taskset[i]["Type"]
-            if typei == 0:      # for low task, only stable mode sched(MID) needs to be guaranteed
-                Ri = rta_mid(i, taskset)
-            else:
-                Ri = rta_low_to_mid(i, taskset)
-            if (Ri > Di):
-                method2_feasible = False
-                if (typei == 1):
-                    method2_feasible_high = False
-                # print("Error #4.")
-            elif (Ri <= Di and typei == 0):
-                low_count += 1
-    # if any high task is infeasible, then the result is invalid
-    if not method2_feasible_high:
-        low_count = 0
-
-    # print(method1_feasible, method2_feasible)
-
-    return method1_feasible, method2_feasible, low_count, low_all
+    return taskset, taskidx, taskidx_high, taskidx_low
 
 
-def trial_searching(target_util):
-    taskset = []
-    taskidx = []
-    taskidx_high = []
-    taskidx_low = []
-
-    # generate the taskset
-    U = UUniFast(number_of_tasks, target_util)
-    # print(U)
-
-    sum_U = 0
-
-    for U_i in U:
-        T_i = random.choice(periods)
-        C_i = math.floor(U_i * T_i)
-        if C_i < 1:
-            C_i = 1
-        sum_U += C_i / T_i
-        task_i = {"Type": 0, "C_low": C_i, "C_mid": 0, "C_high": 0, "T_i": T_i}
-
-        # append through deadline monotonic
-        found = False
-        for j in range(len(taskset)):
-            if T_i <= taskset[j]["T_i"]:
-                taskset.insert(j, task_i)
-                found = True
-                break
-        if not taskset or not found:
-            taskset.append(task_i)
-
-    # print("Total util: ", sum_U)
-
-    # select the HI tasks
-    for i in range(number_of_tasks):
-        taskidx.append(i)
-    idx_high_ = random.sample(taskidx, number_of_hi_tasks)
-
-    for i in range(number_of_tasks):
-        if i in idx_high_:
-            taskset[i]["Type"] = 1
-            taskset[i]["C_high"] = math.ceil(taskset[i]["C_low"] * C_high_multiplier)
-            taskidx_high.append(i)
-        else:
-            taskset[i]["C_mid"] = math.ceil(taskset[i]["C_low"] * C_low_Mi)
-            taskidx_low.append(i)
-
-    # check schedulability of low mode; if not then discard (discard skipped)
-    for i in range(number_of_tasks):
-        Di = taskset[i]["T_i"]
-        Ri = rta_low(i, taskset)
-        if (Ri > Di):
-            # print("Error #1.")
-            pass
+def trial_searching(taskset_, taskidx, taskidx_high, taskidx_low, search_gamma=True, gamma_in=-1):
+    taskset = taskset_.copy()
 
     # --------------------------------------------------------------------------------------------
     # searching
     # --------------------------------------------------------------------------------------------
-    # generate the gamma set
-    gamma_set = []
+    if search_gamma:
+        # generate the gamma set
+        # gamma_set = []
+        #
+        # step = 0.01
+        # gamma_this = 0
+        # while gamma_this < 1:
+        #     gamma_set.append(gamma_this)
+        #     gamma_this += step
 
-    step = 0.01
-    gamma_this = 0
+        gamma_set = [x * 0.01 for x in range(1, 100)]
 
-    while gamma_this < 1:
-        gamma_set.append(gamma_this)
-        gamma_this += step
+        # calculate C_i^MD based on gamma
+        L = 0
+        R = len(gamma_set) - 1
 
-    # calculate C_i^MD based on gamma
-    L = 0
-    R = len(gamma_set) - 1
+        while L <= (R - 2):
+            M = math.ceil((R + L) / 2)
+            gamma = gamma_set[M]
 
-    while L <= (R - 2):
-        M = math.ceil((R + L) / 2)
-        gamma = gamma_set[M]
+            # calculate C_i^MID
+            for i in taskidx_high:
+                taskset[i]["C_mid"] = taskset[i]["C_low"] + math.ceil((taskset[i]["C_high"] - taskset[i]["C_low"]) * gamma)
+
+            # test scheduablity
+            scheduable = True
+
+            # check sched(MID) for all tasks
+            for i in taskidx:
+                Di = taskset[i]["T_i"]
+                typei = taskset[i]["Type"]
+                if typei == 0:      # for low task, only stable mode sched(MID) needs to be guaranteed
+                    Ri = rta_mid(i, taskset)
+                else:               # for high task, that are two transiation modes
+                    Ri_l2m = rta_low_to_mid(i, taskset)
+                    Ri_m2h = rta_mid_to_high(i, taskset)
+                    Ri = max(Ri_l2m, Ri_m2h)
+                if (Ri >= Di):
+                    scheduable = False
+                elif (Ri <= Di and typei == 0):
+                    pass
+
+            if scheduable:
+                L = M
+            else:
+                R = M
+    else:
+        gamma = gamma_in
 
         # calculate C_i^MID
         for i in taskidx_high:
             taskset[i]["C_mid"] = taskset[i]["C_low"] + math.ceil((taskset[i]["C_high"] - taskset[i]["C_low"]) * gamma)
-
-        method2_feasible = True
-
-        # check sched(MID) for all tasks
-        for i in taskidx:
-            Di = taskset[i]["T_i"]
-            typei = taskset[i]["Type"]
-            if typei == 0:      # for low task, only stable mode sched(MID) needs to be guaranteed
-                Ri = rta_mid(i, taskset)
-            else:               # for high task, that are two transiation modes
-                Ri_l2m = rta_low_to_mid(i, taskset)
-                Ri_m2h = rta_mid_to_high(i, taskset)
-                Ri = max(Ri_l2m, Ri_m2h)
-            if (Ri >= Di):
-                method2_feasible = False
-            elif (Ri <= Di and typei == 0):
-                pass
-
-        if method2_feasible:
-            L = M + 1
-        else:
-            R = M
+            # check sched(MID) for all tasks
+            for i in taskidx:
+                Di = taskset[i]["T_i"]
+                typei = taskset[i]["Type"]
+                if typei == 0:      # for low task, only stable mode sched(MID) needs to be guaranteed
+                    Ri = rta_mid(i, taskset)
+                else:               # for high task, that are two transiation modes
+                    Ri_l2m = rta_low_to_mid(i, taskset)
+                    Ri_m2h = rta_mid_to_high(i, taskset)
+                    Ri = max(Ri_l2m, Ri_m2h)
+                if (Ri >= Di):
+                    return 0, 0, 0, 0, gamma  #  scheduable = False
+                elif (Ri <= Di and typei == 0):
+                    pass
 
     # --------------------------------------------------------------------------------------------
     # pprint.pprint(taskset)
@@ -544,24 +457,6 @@ def taskset_gen(target_util):
 # Main starts here
 ########################################################################################################################
 if __name__ == "__main__":
-    # Exp 1.
-    # for util_this in [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9]:
-    #     for gamma_this in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-    #         target_util = util_this
-    #         count = 0
-    #         count_low = 0
-    #         count_low_all = 0
-    #
-    #         for k in range(number_of_trials):
-    #             method1_feasible, method2_feasible, low_count, low_all = trial(util_this, gamma_this)
-    #             if not method1_feasible and method2_feasible:
-    #                 count += 1
-    #             count_low += low_count
-    #             count_low_all += low_all
-    #
-    #         print("{0},{1},{2},{3},{4}".format(util_this, gamma_this, count, count_low, count_low_all))
-
-    # Exp 2.
     for util_this in [0.10, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90]:
         target_util = util_this
         count1 = 0
@@ -574,35 +469,35 @@ if __name__ == "__main__":
         count_low_all = 0
 
         for k in range(number_of_trials):
+            # generate the taskset
+            taskset, taskidx, taskidx_high, taskidx_low = task_gen(util_this)
+
             # Optimal
-            ret_method1, ret_method2, low_count, low_all, gamma_this = trial_searching(util_this)
+            method1_feasible, method2_feasible, low_count, low_all, gamma_this = trial_searching(taskset, taskidx, taskidx_high, taskidx_low)
             #print(round(gamma_this, 2))
 
-            if ret_method1:
+            if method1_feasible:
                 count1 += 1
-            if ret_method2:
+            if method2_feasible:
                 count2 += 1
             count_low += low_count
             count_low_all += low_all
 
-            # compare gamma 1
-            gamma_this = 0.1
-            method1_feasible, method2_feasible, low_count, low_all = trial(util_this, gamma_this)
-            count_low_1 += low_count
+            # compare fixed gammas
+            method1_feasible, method2_feasible, low_count, low_all, _ = trial_searching(taskset, taskidx, taskidx_high, taskidx_low, search_gamma=False, gamma_in=0.1)
+            if method2_feasible:
+                count_low_1 += low_count
 
-            # compare gamma 2
-            gamma_this = 0.3
-            method1_feasible, method2_feasible, low_count, low_all = trial(util_this, gamma_this)
-            count_low_2 += low_count
+            method1_feasible, method2_feasible, low_count, low_all, _ = trial_searching(taskset, taskidx, taskidx_high, taskidx_low, search_gamma=False, gamma_in=0.3)
+            if method2_feasible:
+                count_low_2 += low_count
 
-            # compare gamma 3
-            gamma_this = 0.5
-            method1_feasible, method2_feasible, low_count, low_all = trial(util_this, gamma_this)
-            count_low_3 += low_count
+            method1_feasible, method2_feasible, low_count, low_all, _ = trial_searching(taskset, taskidx, taskidx_high, taskidx_low, search_gamma=False, gamma_in=0.5)
+            if method2_feasible:
+                count_low_3 += low_count
 
-            # compare gamma 4
-            gamma_this = 0.7
-            method1_feasible, method2_feasible, low_count, low_all = trial(util_this, gamma_this)
-            count_low_4 += low_count
+            method1_feasible, method2_feasible, low_count, low_all, _ = trial_searching(taskset, taskidx, taskidx_high, taskidx_low, search_gamma=False, gamma_in=0.7)
+            if method2_feasible:
+                count_low_4 += low_count
 
         print("{0},{1},{2},{3},{4},{5},{6},{7},{8}".format(util_this, count1, count2, count_low_1, count_low_2, count_low_3, count_low_4, count_low, count_low_all))
--- plot2.m
+++ plot2.m
@@ -1,7 +1,7 @@
 close all
 clear
 
-data = load("output/data2_new.txt")
+data = load("output/data2_new.txt");
 
 trials_num = 1000;
 
@@ -14,7 +14,7 @@ x = data(:,1);
 y = data(:,4) ./ low_task_num_v;
 z = data(:,5) ./ low_task_num_v;
 k = data(:,6) ./ low_task_num_v;
-%m = data(:,7) ./ low_task_num_v;
+m = data(:,7) ./ low_task_num_v;
 n = data(:,8) ./ low_task_num_v;
 
 % plot survivability
@@ -30,7 +30,7 @@ plot(x, k, 'k^:')
 
 hold on
 
-%plot(x, m, 'k+--')
+plot(x, m, 'k+--')
 
 hold on
 
@@ -39,6 +39,6 @@ plot(x, n, 'kx-')
 
 xlabel('Σ Util');
 ylabel('Survivability');
-legend(["gamma = 0.1", "gamma = 0.3", "gamma = 0.5", "optimal"])
+legend(["\gamma = 0.1", "\gamma = 0.3", "\gamma = 0.5", "\gamma = 0.7", "optimal"])
 
 grid on
